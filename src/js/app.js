2589682d7b31e8f4e6482dabe0aeecfdcc0192ee
klekkwedge <antonov.andrey.10101@gmail.com>
2022-05-25 22:04:45 +0300

    build: update deploy

--- src/js/app.js
+++
@@ -1,9 +0,0 @@
-import burger from './modules/burger.js';
-import animationScroll from './modules/anim-scroll.js';
-import isWebp from './modules/webp.js';
-
-window.addEventListener('DOMContentLoaded', () => {
-  burger();
-  isWebp();
-  animationScroll();
-});
